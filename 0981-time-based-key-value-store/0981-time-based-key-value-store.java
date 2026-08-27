b2eefbf5c09cf899ca92fb9b423d7a0cc1451c44
Shubham Prasad <SHUBHAMPATEL.4124@GMAIL.COM>
2026-08-27 22:19:53 +0530

    Time: 127 ms (94.19%), Space: 108.1 MB (81.04%) - LeetHub

---
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.java
@@ -0,0 +1,74 @@
+class TimeStampedValue {
+  public int timestamp;
+  public String value;
+
+  public TimeStampedValue(int timestamp, String value) {
+    this.timestamp = timestamp;
+    this.value = value;
+  }
+}
+
+class TimeMap {
+  Map<String, ArrayList<TimeStampedValue>> entriesByKey;
+
+  public TimeMap() {
+    // Constructor to initialize the TimeMap object
+    entriesByKey = new HashMap<>();
+  }
+
+  public void set(String key, String value, int timestamp) {
+    // Implementation for setting the value at a specific timestamp
+    if(!entriesByKey.containsKey(key)) {
+      entriesByKey.put(key, new ArrayList<>());
+    }
+    ArrayList<TimeStampedValue> timeStampedValues = entriesByKey.get(key);
+    timeStampedValues.add(new TimeStampedValue(timestamp, value));
+  }
+
+  public String get(String key, int timestamp) {
+    if(!entriesByKey.containsKey(key)) return "";
+
+    ArrayList<TimeStampedValue> timeStampedValues = entriesByKey.get(key);
+       TimeStampedValue timeStamp =
+        binarySearchTimestamp(timeStampedValues, timestamp);
+    if(timeStamp==null) {
+      return "";
+    }
+
+    return timeStamp.value;
+  }
+
+  private TimeStampedValue binarySearchTimestamp(
+      ArrayList<TimeStampedValue> arr, int target) {
+    int left = 0, right = arr.size() - 1;
+    int matchIndex = -1;
+
+    while (left <= right) {
+      int mid = left + (right - left) / 2;
+      TimeStampedValue cur = arr.get(mid);
+      if(cur.timestamp == target){
+          matchIndex = mid;
+          return arr.get(matchIndex);
+      }
+      else if(cur.timestamp < target) {
+        matchIndex = mid;
+        left = mid + 1;
+      }
+      else {
+        right = mid - 1;
+      }
+    }
+
+    if(matchIndex == -1) {
+      return null;
+    }
+    return arr.get(matchIndex);
+  }
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
