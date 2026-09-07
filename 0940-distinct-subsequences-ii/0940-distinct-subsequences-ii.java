505a757d53b79e9fa3f51c1ce0884575af528603
Shubham Prasad <SHUBHAMPATEL.4124@GMAIL.COM>
2026-09-07 19:47:06 +0530

    Time: 3 ms (89.36%), Space: 42.9 MB (98.23%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
@@ -1,48 +1,35 @@
 class Solution {
-
-    int M = 1_000_000_007;
-    int[] dp = new int[2001];
+    int M = 1000000007;
+    int[] dp;
     int[] prev;
-
-    int solve(int n) {
-
-        if (n == 0)
-            return 1;
-
-        if (dp[n] != -1)
-            return dp[n];
-
-        int total = (int) ((2L * solve(n - 1)) % M);
-
-        if (prev[n] != 0) {
-            int duplicates = solve(prev[n] - 1);
-
-            total = (total - duplicates + M) % M;
-        }
-
-        return dp[n] = total;
-    }
-
     public int distinctSubseqII(String s) {
-
         int n = s.length();
-
-        // Fill dp with -1
-        Arrays.fill(dp, -1);
-
-        prev = new int[n + 1];
-
-        int[] lastSeen = new int[26];
-
-        for (int i = 1; i <= n; i++) {
-
-            int idx = s.charAt(i - 1) - 'a';
-
-            prev[i] = lastSeen[idx];
-
-            lastSeen[idx] = i;
+        dp=new int[n+1];
+        for(int i=0;i<dp.length;i++){
+            dp[i]=-1;
         }
-
-        return (solve(n) - 1 + M) % M;
+        prev=new int[n+1];
+        int[] lastseen = new int[26];
+        for(int i=1;i<=n;i++){
+            int idx = s.charAt(i-1)-'a';
+            prev[i]=lastseen[idx];
+            lastseen[idx]=i;
+        }
+        return ((solve(n)-1)+M)%M;
     }
+    public int solve(int n){
+        // base case 
+        if(n==0){
+           return 1;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        int total = (int)(2L * solve(n-1))%M;
+        if(prev[n]!=0){
+            total = (total-solve(prev[n]-1)+M)%M;
+        }
+        return dp[n]=total;
+    }
+    
 }
