b01a6bec2a900aa264de4c7c59dddfaa88d4034d
Shubham Prasad <SHUBHAMPATEL.4124@GMAIL.COM>
2026-09-08 14:00:23 +0530

    Time: 1 ms (83.65%), Space: 42.6 MB (39.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -3,10 +3,6 @@ class Solution {
         if(n<1000){
             return 0;
         }
-        int count = 0;
-        for(int i=1000;i<=n;i++){
-            count++;
-        }
-        return count;
+        return (n-1000)+1;
     }
 }
