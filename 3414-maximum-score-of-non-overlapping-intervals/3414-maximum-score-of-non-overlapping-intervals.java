e2790b8ce270b9e05b5703dfb98947b5064d3ade
Shubham Prasad <SHUBHAMPATEL.4124@GMAIL.COM>
2026-09-12 18:23:50 +0530

    Time: 133 ms (85.18%), Space: 220.4 MB (25.92%) - LeetHub

--- 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.java
+++ 3414-maximum-score-of-non-overlapping-intervals/3414-maximum-score-of-non-overlapping-intervals.java
@@ -81,12 +81,7 @@ class Solution {
             intervals[i][3] = i;
         }
 
-        Arrays.sort(intervals, (a, b) -> {
-            if (a[0] != b[0]) return a[0] - b[0];
-            if (a[1] != b[1]) return a[1] - b[1];
-            if (a[2] != b[2]) return a[2] - b[2];
-            return a[3] - b[3];
-        });
+        Arrays.sort(intervals, (a, b) ->Integer.compare(a[0],b[0]));
 
         nextIdx = new int[n];
         for (int i = 0; i < n; i++) {
