d762ed1c7da55acd4ce7edf449025438b759cf11
Shubham Prasad <SHUBHAMPATEL.4124@GMAIL.COM>
2026-09-18 00:18:03 +0530

    Time: 6 ms (99.1%), Space: 90.9 MB (71.3%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.java
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.java
@@ -1,38 +1,28 @@
 class Solution {
     public int minSumOfLengths(int[] arr, int target) {
         int n = arr.length;
-
+        int[] minBestLengthTillIdx = new int[n];
+        Arrays.fill(minBestLengthTillIdx, Integer.MAX_VALUE);
+        int currsum = 0;
+        int bestMinLength = Integer.MAX_VALUE;
+        int result = Integer.MAX_VALUE;
         int i = 0;
         int j = 0;
-
-        int currSum = 0;
-        int[] minBestLenTillIdx = new int[n];
-        Arrays.fill(minBestLenTillIdx, Integer.MAX_VALUE);
-
-        int bestMinLen = Integer.MAX_VALUE;
-        int result = Integer.MAX_VALUE;
-
         while (j < n) {
-            currSum += arr[j];
-
-            while (i < j && currSum > target) {
-                currSum -= arr[i++];
+            currsum += arr[j];
+            while (i < j && currsum > target) {
+                currsum -= arr[i++];
             }
-
-            if (currSum == target) {
-                int len = j - i + 1;
-
-                if (i > 0 && minBestLenTillIdx[i - 1] != Integer.MAX_VALUE) {
-                    result = Math.min(result, len + minBestLenTillIdx[i - 1]);
+            if (currsum == target) {
+                int length = j - i + 1;
+                if (i > 0 && minBestLengthTillIdx[i - 1] != Integer.MAX_VALUE) {
+                    result = Math.min(result, length + minBestLengthTillIdx[i - 1]);
                 }
-
-                bestMinLen = Math.min(bestMinLen, len);
+                bestMinLength = Math.min(bestMinLength, length);
             }
-
-            minBestLenTillIdx[j] = bestMinLen;
+            minBestLengthTillIdx[j] = bestMinLength;
             j++;
         }
-
         return result == Integer.MAX_VALUE ? -1 : result;
     }
 }
