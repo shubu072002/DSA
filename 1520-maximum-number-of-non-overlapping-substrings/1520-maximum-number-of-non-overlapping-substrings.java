2f1e8ec7f844666b38a1fd832c33c4f1613b6c9a
Shubham Prasad <SHUBHAMPATEL.4124@GMAIL.COM>
2026-09-19 15:29:14 +0530

    Time: 14 ms (38.76%), Space: 48.3 MB (35.4%) - LeetHub

--- 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.java
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.java
@@ -1,45 +1,37 @@
 class Solution {
     public List<String> maxNumOfSubstrings(String s) {
-        int n = s.length();
-
-        int[] start = new int[26];
-        int[] end = new int[26];
-        boolean[] isValid = new boolean[26];
-
-        Arrays.fill(start, -1);
-        Arrays.fill(isValid, true);
-
-        List<String> result = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            int idx = s.charAt(i) - 'a';
-
-            if (start[idx] == -1) {
-                start[idx] = i;
-            }
-            end[idx] = i;
+      int n = s.length();
+      int[] start = new int[26];
+      int[] end = new int[26];
+      List<String> result = new ArrayList<>();
+      boolean[] isvalid = new boolean[26];
+      Arrays.fill(start,-1);
+      Arrays.fill(isvalid,true);
+      for(int i=0;i<n;i++){
+        int charIdx = s.charAt(i)-'a';
+        if(start[charIdx]==-1){
+           start[charIdx]=i;
         }
+        end[charIdx]=i;
+      }  
+      for(int i=0;i<26;i++){
+        if(start[i]==-1)continue;
 
-        for (int c = 0; c < 26; c++) { // checking all characters
-            if (start[c] == -1) continue;
-
-            for (int i = start[c]; i <= end[c]; i++) {
-                int ch = s.charAt(i) - 'a';
-                if (start[ch] < start[c]) {
-                    isValid[c] = false;
-                    break;
-                }
-
-                end[c] = Math.max(end[c], end[ch]);
-            }
+        for(int j=start[i];j<=end[i];j++){
+            int idx = s.charAt(j)-'a';
+            if(start[idx]<start[i]){
+                isvalid[i]=false;
+                break;
+            } 
+            end[i]=Math.max(end[i],end[idx]);
         }
-
-        int lastTakenStart = Integer.MAX_VALUE;
+      }
+       int lastTakenStart = Integer.MAX_VALUE;
 
         for (int i = n - 1; i >= 0; i--) {
             int c = s.charAt(i) - 'a';
 
-            if (!isValid[c]) continue;
+            if (!isvalid[c]) continue;
 
             if (i == start[c] && end[c] < lastTakenStart) {
                 result.add(s.substring(i, end[c] + 1));
